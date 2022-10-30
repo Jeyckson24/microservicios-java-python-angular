97d212cfd8e168d554392433098edb1e2fa2a6ef
mabel lucero <mabelucero4@gmail.com>
2022-10-30 12:45:25 -0500

    Se finaliza CRUD para el ms administracion de partidos se realizan pruebas OK

--- index.py
+++ index.py
@@ -5,9 +5,12 @@ from flask import jsonify
 from waitress import serve
 import json
 from controllers.mesasControler import AdministracionMesas
+from controllers.partidosControler import AdministracionPartidos
+
 mi_app = Flask (__name__)
 
 _controler_usuario = AdministracionMesas()
+_controler_partidos = AdministracionPartidos()
 #**********************administracion Mesas ***************************************
 
 #get
@@ -43,6 +46,43 @@ def eliminar_mesa(id):
     
     return jsonify(datos_salida)
 
+
+#**********************administracion Partidos ***************************************
+
+#get
+
+@mi_app.route('/partidos',methods=['GET'])
+def mostrar_partidos():
+    datos_salida = _controler_partidos.mostrar_partidos()
+    
+    return jsonify(datos_salida)
+#post
+
+@mi_app.route('/partidos',methods=['POST'])
+def crear_partido():
+    datos_entrada = request.get_json()
+    datos_salida = _controler_partidos.crear_partido(datos_entrada)
+    
+    return jsonify(datos_salida)
+#put
+
+@mi_app.route('/partidos/<string:id>',methods=['PUT'])
+def actualizar_partido(id):  
+    datos_entrada =request.get_json()
+    json = _controler_partidos.actualizar_partido (id,datos_entrada)
+    
+    return jsonify(json)
+
+
+#delete
+
+@mi_app.route('/partidos/<string:id>',methods=['DELETE'])
+def eliminar_partido(id):    
+    datos_salida = _controler_partidos.eliminar_partido(id)
+    
+    return jsonify(datos_salida)
+
+
 def cargar_configuracion():
     with open ("config.json") as archivo:
         datos_configuracion = json.load(archivo)
@@ -52,5 +92,3 @@ if __name__ == '__main__':
     datos_configuracion = cargar_configuracion()
     print("Servidor Ejecutandose...")
     serve (mi_app, host=datos_configuracion["servidor"], port=datos_configuracion["puerto"])
-    
-#**********************administracion Partidos ***************************************
---
+++ model/partidos.py
@@ -0,0 +1,5 @@
+from ast import Mod
+from model.modeloAbstracto import ModeloAbstracto
+
+class Partidos (ModeloAbstracto):
+    pass
