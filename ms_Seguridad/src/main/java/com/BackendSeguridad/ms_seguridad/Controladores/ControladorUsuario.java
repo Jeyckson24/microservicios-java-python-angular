40d4d0ef1dc8e82b80c1b60fc913253ab5976ea9
jeycksonCh <jeison24@gmail.com>
2022-11-08 21:00:11 -0500

    Se finaliza el CRUD para el modelo USUARIO se hacen pruebas y es funcional

--- ms_Seguridad/src/main/java/com/BackendSeguridad/ms_seguridad/Controladores/ControladorUsuario.java
+++ ms_Seguridad/src/main/java/com/BackendSeguridad/ms_seguridad/Controladores/ControladorUsuario.java
@@ -20,10 +20,20 @@ public class ControladorUsuario {
     public Usuario crearUsuario(@RequestBody Usuario usuarioEntrada){
         return miRepositorio.save(usuarioEntrada);
     }
-    @DeleteMapping("/eliminar")
-    public String eliminarUsuario(String idUsuario){
+    @DeleteMapping("/{idUsuario}")
+    public String eliminarUsuario(@PathVariable String idUsuario){
         miRepositorio.deleteById(idUsuario);
        return "Usuario" + idUsuario + "eliminado";
+    }
+
+    @PutMapping ("/{idUsuario}")
+    public String actualizarUsuario(@PathVariable String idUsuario, @RequestBody Usuario usuarioEntrada){
+        Usuario usuarioBusqueda = miRepositorio.findById(idUsuario).orElse(null);
+        usuarioBusqueda.setNombre_usuario(usuarioEntrada.getNombre_usuario());
+        usuarioBusqueda.setContrasena(usuarioEntrada.getContrasena());
+        usuarioBusqueda.setCorreo(usuarioEntrada.getCorreo());
+        miRepositorio.save(usuarioBusqueda);
+        return "Usuario" + idUsuario + "actualizado";
     }
 
 
