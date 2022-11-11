beb6c7e6a8180db1d823794040f22ca10e5bb22e
Carolina F <coraline215@gmail.com>
2022-11-10 23:30:33 -0500

    Se finaliza rol

--- ms_Seguridad/src/main/java/com/BackendSeguridad/ms_seguridad/Controladores/ControladorRol.java
+++ ms_Seguridad/src/main/java/com/BackendSeguridad/ms_seguridad/Controladores/ControladorRol.java
@@ -1,1 +1,32 @@
 package com.BackendSeguridad.ms_seguridad.Controladores;
+
+import com.BackendSeguridad.ms_seguridad.Modelos.Rol;
+import com.BackendSeguridad.ms_seguridad.Repositorios.RepositorioRol;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rol")
+public class ControladorRol {
+    @Autowired
+    private RepositorioRol miRepoRol;
+
+    @GetMapping("/listar")
+    public List<Rol> listar(){
+        return miRepoRol.findAll();
+    }
+
+    @PostMapping("/crear")
+    public Rol crearRol(@RequestBody Rol rolEntrada){
+        return miRepoRol.save(rolEntrada);
+    }
+
+    @DeleteMapping("/eliminar")
+    public String eliminarRol(@RequestParam(value = "idRol") String _idRol){
+        miRepoRol.deleteById(_idRol);
+        return "Rol " + _idRol + " eliminado";
+    }
+
+}
