7b7cd5dccf61b753c47c70490386bde2c9fa7962
jeycksonCh <jeison24@gmail.com>
2022-11-19 02:28:42 -0500

    Se configura datos de conexion en api_gateway

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from waitress import serve
 import json
 
 app = Flask(__name__)
@@ -15,5 +16,12 @@ def inicio_sesion():
     return "path login api gateway"
 
 
+def cargar_configuracion():
+    with open ("config.json") as archivo:
+        datos_configuracion = json.load(archivo)
+    return datos_configuracion
+
 if __name__ == '__main__':
-    app.run()
+    datos_configuracion = cargar_configuracion()
+    print("Servidor Ejecutandose... en "+datos_configuracion["url-api-gateway"]+str (datos_configuracion["puerto-api-gateway"]))
+    serve (app, host=datos_configuracion["url-api-gateway"], port=datos_configuracion["puerto-api-gateway"])
