f8f03b68be394d24743ddd3d7a2394ff50a93f14
Jeyckson Chacon <jeison24@gmail.com>
2022-11-19 22:02:14 -0500

    Cambios realizado al API-GATEWAY

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -7,15 +7,19 @@ import json
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Api gateway'
-
-@app.route("/login")
+@app.route("/login",methods=["POST"])
 def inicio_sesion():
+    datos_entrada = request.get_json()
+    configuracion = cargar_configuracion()
+    respuesta = request.post(configuracion["url-ms-seguridad"]+"/user/login",json=datos_entrada)
+    print(configuracion["url-ms-seguridad"]+"/usuarios/login")
+    print(respuesta.status_code)
+    print(respuesta.json())
     return "path login api gateway"
 
-
+@app.route('/')
+def home ():
+    return 'api gateway'
 def cargar_configuracion():
     with open ("config.json") as archivo:
         datos_configuracion = json.load(archivo)
